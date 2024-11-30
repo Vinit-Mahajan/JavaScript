808f1059929b93b8f4f6065a980ce2ce50e02c04
Vinit-Mahajan <mvinit923@gmail.com>
2024-11-30 13:15:37 +0530

    datatypes

---
+++ Dataytypes/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(1+2) // we are usingn nodejs to run not browser
+
+let name = "vinit"
+let age = 24
+let isMale = true
+
+
+// number => 2 ^ 54
+// BigInt => greater than number
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => variable is decalred but not assigned value to it e.g. : let name;
+// Symbol => unique
+// object=>
+
+console.log(typeof "vinit");
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       //object
+
+
