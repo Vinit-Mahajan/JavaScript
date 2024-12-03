69740b98da998a25a5150099b302e3ab46ccf7c4
Vinit-Mahajan <mvinit923@gmail.com>
2024-12-03 22:17:04 +0530

    operations in js

---
+++ Dataytypes/operations.js
@@ -0,0 +1,38 @@
+let no = 11
+let negno = -no
+// console.log(negno);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);               //calculate power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " vinit"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+let a = ("1" + 2);
+let b = (1 + "2");
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+
+// console.log("1" + 2 + 3);
+// console.log(3 + 2 + "1");
+// console.log(+true);
+// console.log(+"");
+
+
+
+
+
+
+
+
+
+
