e364bd110958e573d2bfa4826cf92275a89316a9
Vinit-Mahajan <mvinit923@gmail.com>
2024-12-03 21:56:59 +0530

    datatype conversion

---
+++ Dataytypes/datatypeconversion.js
@@ -0,0 +1,39 @@
+let score = 33
+
+// console.log("score : ", typeof(score)); //number
+
+let no1 = "33"
+// console.log("no1 : ", typeof(no1));
+
+let num = Number(no1)
+// console.log("num : ", num);
+
+let no2 = null  //"12dad"
+// console.log("no2 : ", typeof(no2));
+// console.log("no2 : ", no2);
+
+let num2 = Number(no2)
+// console.log("num2 : ", typeof(num2));
+// console.log("num2 : ", num2);           //NaN
+
+// "32" => 33
+// "32avs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = -1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// "" => false; "ahjs" => true
+//12 = true ; 0 => false
+
+let no = 1234
+let strNumber = String(no)
+console.log(strNumber);
+console.log(typeof(strNumber));
+
+
+
+
+
+
