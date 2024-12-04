b3c0f97ae384a6c32d9fcadfc78e042a68fa04b0
Vinit-Mahajan <mvinit923@gmail.com>
2024-12-04 22:07:20 +0530

    comparison done

---
+++ Dataytypes/comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// === => Strict Check (not onlyc checks the value but also checks for its data type)
+
+console.log("2" === 2);
