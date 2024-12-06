393ab766375fba9466c3fce84de59cf088f0beea
Vinit-Mahajan <mvinit923@gmail.com>
2024-12-06 22:59:30 +0530

    string basic

---
+++ Strings/string_demo.js
@@ -0,0 +1,20 @@
+let name = "vinit"
+let last_name = " mahajan"
+
+// console.log(name + last_name)
+
+console.log(`my name is ${name + last_name} `);
+
+const my_name = new String("vinit mahajan")
+console.log(typeof(my_name));
+console.log(my_name[1]);
+console.log(my_name.__proto__);
+console.log(my_name.length);
+console.log(my_name.toUpperCase());
+
+
+// const my_name2 = name
+// console.log(typeof(my_name2));
+
+
+
