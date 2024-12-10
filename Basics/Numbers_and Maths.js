068457235d932edb057b4ad331a84eff16f94830
Vinit-Mahajan <mvinit923@gmail.com>
2024-12-11 00:06:42 +0530

    String summary

---
+++ Basics/Numbers_and Maths.js
@@ -0,0 +1,13 @@
+const score = 999
+console.log(score);
+
+const fd_amt = new Number(100000)
+console.log(fd_amt);
+
+console.log(fd_amt.toFixed(2));
+console.log(fd_amt.toString().length);
+
+
+
+
+
