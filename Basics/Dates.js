91e3e0db994279def4c5767aafcc6ba1fa63953e
Vinit-Mahajan <mvinit923@gmail.com>
2024-12-18 23:20:28 +0530

    Date and Time learned

---
+++ Basics/Dates.js
@@ -0,0 +1,25 @@
+let myDate = new Date()
+
+// console.log("toString : ", myDate.toString());
+// console.log("toDateString : ", myDate.toDateString());
+// console.log("toISOString : ", myDate.toISOString());
+// console.log("toJSON : ", myDate.toJSON());
+// console.log("toLocaleDateString : ", myDate.toLocaleDateString());
+// console.log("toLocaleString : ", myDate.toLocaleString());
+// console.log("toUTCString : ", myDate.toUTCString());
+// console.log(typeof(myDate));
+
+// let myCreatedDate = new Date(2024, 11, 13)      //Year, Month(starts from 0), day
+// // console.log(myCreatedDate.toLocaleString());
+
+// let myStamp = Date.now()
+// console.log(myStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log("newDate : ", newDate);
+console.log(".getMonth : ", newDate.getMonth());
+console.log("");
+console.log();
+console.log();
